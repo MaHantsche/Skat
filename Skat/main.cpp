a5c2339565eef57560536804a35029d22cfe039b
Marcel Hantsche <marcelhantsche@marcels-macbook-pro.local>
2018-12-14 20:31:09 +0100

    Initial Commit

---
+++ Skat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Skat
+//
+//  Created by Marcel Hantsche on 14.12.18.
+//  Copyright © 2018 Marcel Hantsche. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
